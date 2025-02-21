deaf1d3383b1a99c94038e0be5bb5354c6680de4
Thiago Rovina <ra124257@uem.br>
2025-02-20 21:43:24 -0300

    teste2

---
+++ javaSpring-main/javaSpring-main/demo (1)/demo/src/main/java/com/battlefield/demo/produtos/dao/ProdutosDAO.java
@@ -0,0 +1,60 @@
+package com.battlefield.demo.produtos.dao;
+
+import com.battlefield.demo.dao.GenericDAO;
+import com.battlefield.demo.produtos.model.Produtos;
+import com.battlefield.demo.usuario.model.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NoResultException;
+import java.util.List;
+
+public class ProdutosDAO  extends GenericDAO<Produtos, Integer> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public ProdutosDAO() {
+        super(Produtos.class);
+    }
+
+    public Produtos buscaProduto(Integer id, String nome) {
+        try {
+            return entityManager
+                    .createQuery("SELECT P1.idProduto, P1.nmProduto FROM  Produtos p1", Produtos.class)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    public List<Usuario> listarTodos(String filtro, String ordem) {
+        try {
+            return entityManager.createQuery("FROM Produtos  WHERE " + filtro + " " + ordem, Usuario.class)
+                    .getResultList();
+        } catch (Exception e) {
+            System.out.println("------ERRO produtosDAO-----\n" + e.getMessage());
+            return null;
+        }
+    }
+
+    @Transactional
+    public void gravar(Produtos produtos) {
+        if (produtos.getIdProduto() == null) {
+            entityManager.persist(produtos);
+        } else {
+            entityManager.merge(produtos);
+        }
+    }
+
+    public enum TipoOcorrenciaLog {
+        INSERCAO,
+        ALTERACAO,
+        EXCLUSAO
+    }
+
+    public static void insereLog(String entidade, ProdutosDAO.TipoOcorrenciaLog tipoOcorrencia) {
+        System.out.println("Log: " + tipoOcorrencia + " na entidade: " + entidade);
+    }
+}
